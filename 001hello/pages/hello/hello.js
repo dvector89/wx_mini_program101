850bfc6dd0f720abaf0844c0053b8027e36bfe6e
dvector89 <shiliangliang.cas@gmail.com>
2020-08-02 20:39:32 +0800

    001 hello: import utils.js and get time

--- 001hello/pages/hello/hello.js
+++ 001hello/pages/hello/hello.js
@@ -1,1 +1,13 @@
-Page({})
+var utils = require('../../utils/utils.js');
+// util vs utils: typo error
+
+Page({
+  data: {},
+  // 由于缓存的原因，真机调试的时候，显示可能会保持上次的结果。需要在手机上，点击重新进入
+  onLoad: function() {
+    var time = utils.formatTime(new Date());
+    this.setData({
+      time: time
+    });
+  }
+})
